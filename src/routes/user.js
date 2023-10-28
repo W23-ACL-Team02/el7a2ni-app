02c6b8dbbf0c49a72c55e6cd9c83846c1a074594
FadluW <fadlu.w@gmail.com>
2023-10-28 22:43:11 +0200

    * Added login post and get routes * Added login view * Added register route for doctor * Added express session middleware

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,22 +8,22 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register/doctor', async (req, res) => {
-    const {username, name, email, password, dateOfBirth, hourlyRate, affiliation, education_name, education_end} = req.body;
-    const education = {
-        name: education_name,
-        endYear: education_end.split("-")[0]
-    }
-    const type = "doctor";
-    const isAccepted = false;
+  const {username, name, email, password, dateOfBirth, hourlyRate, affiliation, education_name, education_end} = req.body;
+  const education = {
+    name: education_name,
+    endYear: education_end.split("-")[0]
+  }
+  const type = "doctor";
+  const isAccepted = false;
 
-    try {
-        const user = await userModel.create({username, name, email, password, dateOfBirth, hourlyRate, affiliation, education, type, isAccepted});
-        await user.save();
-    
-        res.status(200).send(`Doctor ${user.username} created successfully!`);
-      } catch (error) {
-        res.status(400).json({err:error.message});
-      }
+  try {
+    const user = await userModel.create({username, name, email, password, dateOfBirth, hourlyRate, affiliation, education, type, isAccepted});
+    await user.save();
+
+    res.status(200).send(`Doctor ${user.username} created successfully!`);
+  } catch (error) {
+    res.status(400).json({err:error.message});
+  }
 });
 
 router.post('/register/patient', async (req, res) => {
@@ -36,8 +36,6 @@ router.post('/register/patient', async (req, res) => {
   };
   const type = "patient";
 
-
-
   try {
     const user = await userModel.create({username, name, email, password, dateOfBirth, gender, mobile, type, emergencyContact});
     await user.save();
@@ -48,5 +46,32 @@ router.post('/register/patient', async (req, res) => {
   }
   // res.send('Registered patient');
 });
+
+router.post('/login', async (req, res) => {
+  const {username, password} = req.body;
+  
+  try {
+    // Check for user in database
+    const user = await userModel.findOne({username: username});
+    
+    if (!user || password != user.password || user.type == 'pharmacist') {
+      // If not found reload page with error message
+      return res.redirect('/login')
+
+    } else if (user?.type == 'doctor' && !user.isAccepted) {
+      return res.status(400).send(`Doctor ${user.name} not yet approved.`)
+
+    } else {
+      // Else load session variables
+      req.session.loggedin = true;
+      req.session.userId = user?._id;
+      req.session.userType = user?.type;
+
+      return res.redirect('/home');
+    }
+  } catch (error) {
+    res.status(400).json({err:error.message});
+  }
+})
 
 module.exports = router;
