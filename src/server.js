3d0fa5a54fe5b7f9ab38663cefdc6c86e1e870fa
FadluW <fadlu.w@gmail.com>
2023-12-03 21:54:23 +0100

    added cors + externalized port & mongo

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+const MongoURI = process.env.MONGO_URI;
+const FgCyan = "\x1b[36m"
+const FgGreen = "\x1b[32m"
+const FgWhite = "\x1b[37m"
+
+// Set port
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+// Mongo DB
+mongoose.set('strictQuery', false);
+mongoose.connect(MongoURI)
+.then(()=>{
+    console.log(`${FgGreen}[DB] MongoDB is now connected!${FgWhite}`, )
+    // Starting server
+    app.listen(port, () => {
+        console.log(`${FgCyan}[API] Listening to requests on http://localhost:${port}${FgWhite}`);
+    })
+})
+.catch(err => console.log(err));
