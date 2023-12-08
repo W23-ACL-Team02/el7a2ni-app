744d1a2d80a36ac03d99a600a201690a0be16189
MoKhatib2 <mohamedahmed1902@yahoo.com>
2023-12-08 19:01:23 +0100

    merged with omar's branch

--- src/routes/private.js
+++ src/routes/private.js
@@ -6,14 +6,8 @@ var adminRouter = require('./private/admin');
 var familyMemberRouter = require('./private/familymember');
 var doctorRouter = require('./private/doctors');
 var prescriptionRouter= require('./private/prescription');
-<<<<<<< HEAD
 var doctor = require("./private/doctor");
 var payment = require("./private/payment")
-=======
-var doctorController = require("./private/doctorController");
-var familyRouter = require(`./private/family`)
-var patientRouter = require("./private/patient");
->>>>>>> b04cfa0ad03fcdb8f1176d87c0ab48981431980c
 const authentiateToken = require("../middleware/authenticateToken");
 const patientController = require('./private/patient');
 
@@ -32,12 +26,8 @@ router.use('/family', familyRouter);
 router.use('/familymember', familyMemberRouter);
 router.use('/doctors', [doctorRouter, doctorController]);
 router.use('/prescription',prescriptionRouter);
-<<<<<<< HEAD
 router.use('/doctor', doctor);
 router.use('/payment', payment)
-=======
-router.use('/patients', doctorController)
 router.use('/patient', patientRouter)
->>>>>>> b04cfa0ad03fcdb8f1176d87c0ab48981431980c
 
 module.exports = router;
