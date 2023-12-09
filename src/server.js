f958766ec1809d2991879759182dfcedeeb8dbc7
FadluW <fadlu.w@gmail.com>
2023-12-09 16:01:22 +0000

    remove cors from server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const app = require('./app');
-var cors = require('cors');
 const MongoURI = process.env.MONGO_URI;
 const FgCyan = "\x1b[36m"
 const FgGreen = "\x1b[32m"
@@ -9,7 +8,6 @@ const FgWhite = "\x1b[37m"
 // Set port
 var port = process.env.PORT || '3000';
 app.set('port', port);
-app.use(cors());
 
 // Mongo DB
 mongoose.set('strictQuery', false);
