1c394994f12bfb4ceac914568312ef643da6422b
FadluW <fadlu.w@gmail.com>
2023-12-31 16:52:39 +0200

    Notification Templates

---
+++ src/handlers/notification/templates/medicineTemplate.js
@@ -0,0 +1,9 @@
+const medicineBodyOutOfStock = () => {
+    return `MEDICINE OUT OF STOCK`
+}
+
+const medicineTitleOutOfStock = () => {
+    return `MEDICINE TITLE OUT OF STOCK`
+}
+
+module.exports = { medicineBodyOutOfStock, medicineTitleOutOfStock }
