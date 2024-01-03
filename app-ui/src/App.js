81bc693b0af14f9cd1370cafcb14f2ef3cbe1f8d
MoKhatib2 <mohamedahmed1902@yahoo.com>
2024-01-03 15:44:55 +0100

    intial commit

--- app-ui/src/App.js
+++ app-ui/src/App.js
@@ -2,14 +2,24 @@ import { useState } from 'react';
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-
+import Login from '../src/components-main/Login';
+import PatientsList from '../src/components-clinic/doctor/PatientsList'
+import PatientDetails from '../src/components-clinic/doctor/PatientDetails'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
     <div className="App">
-    
+      <BrowserRouter>
+        <Routes>
+        {/*  */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/patientsList" element={<PatientsList />} />
+          <Route path="/patient-details" element={<PatientDetails />} />
+          <Route path="/VideoCallRoom" element={<VideoCallRoom />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
