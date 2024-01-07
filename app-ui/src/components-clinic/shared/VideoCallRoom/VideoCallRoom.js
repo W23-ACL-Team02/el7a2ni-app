ccb234af7b6fc4474126021d4a0d5cd179a904fa
MoKhatib2 <mohamedahmed1902@yahoo.com>
2024-01-07 16:43:04 +0100

    some updates

--- app-ui/src/components-clinic/shared/VideoCallRoom/VideoCallRoom.js
+++ app-ui/src/components-clinic/shared/VideoCallRoom/VideoCallRoom.js
@@ -7,6 +7,7 @@ import { Select } from '@mui/base/Select';
 import { Option } from '@mui/base/Option';
 import AssignmentIcon from "@mui/icons-material/Assignment"
 import PhoneIcon from "@mui/icons-material/Phone"
+import CircleIcon from '@mui/icons-material/Circle';
 import React, { useEffect, useRef, useState } from "react"
 import { CopyToClipboard } from "react-copy-to-clipboard"
 import Peer from "simple-peer"
@@ -198,68 +199,69 @@ const VideoCallRoom = () => {
 
     return (
         <div className={styles.mainContainer}>
-			<h1 style={{ textAlign: "center", color: '#fff' }}>Clinic Video Chat Room</h1>
-		<div className="container">
-			<div className={styles.video_container}>
-				<div className={styles.video}>
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
-				</div>
-				<div className={styles.video}>
-					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
-					null}
-				</div>
-			</div>
-			<div className={styles.myId}>
-				<TextField
-					id="filled-basic"
-					label="Name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					style={{ marginBottom: "20px" }}
-				/>
-				{/* {<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-					<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-						Copy ID
-					</Button>
-				</CopyToClipboard>} */}
-				<div>
-					<h3>Online Users:</h3>
-					
-				<select defaultValue={idToCall} id="named-select" name="user-select"  onChange={handleChange} style={{ width: "120px" }}>
-					{onlineUsers.map((user) => (
-						<option value={user.id}>
-                			{user.username}
-           			 	</option>
-					))}
-  				</select>
-
-      			</div>		
-				<div className={styles.call_button}>
-					{callAccepted && !callEnded ? (
-						<Button variant="contained" color="secondary" onClick={leaveCall}>
-							End Call
-						</Button>
-					) : (
-						<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-							<PhoneIcon fontSize="large" />
-						</IconButton>
-					)}
-					{/* {idToCall} */}
-				</div>
-			</div>
-			<div>
-				{receivingCall && !callAccepted ? (
-						<div className={styles.caller}>
-						<h1 >{name} is calling...</h1>
-						<Button variant="contained" color="primary" onClick={answerCall}>
-							Answer
-						</Button>
+			<h1 style={{ textAlign: "center", color: '#000' }}>Clinic Video Chat Room</h1>
+			<div className={styles.container}>
+				<div className={styles.video_container}>
+					<div className={styles.video}>
+						{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "700px" , height: '600px'}} />}
 					</div>
-				) : null}
+					<div className={styles.video}>
+						{callAccepted && !callEnded ?
+						<video playsInline ref={userVideo} autoPlay style={{ width: "700px", height: '600px'}} />:
+						null}
+					</div>
+				</div>
+				<div className={styles.call_container}>
+					{/* <label
+						id="filled-basic"
+						label="Name"
+						variant="filled"
+						style={{ marginBottom: "20px" , color: '#000'}}
+					> {name} </label>
+					<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+						<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
+							Copy ID
+						</Button>
+					</CopyToClipboard> */}
+					<div>
+						<div className={styles.UsersLabel}>
+							<CircleIcon fontSize="small" style={{ color:"green"}}/>
+							<h3>Online Users:</h3>
+						</div>
+						<div className={styles.OnlineUsers}>
+							<select defaultValue={idToCall} id="named-select" name="user-select"  onChange={handleChange} style={{ width: '180px'}}>
+								{onlineUsers.map((user) => (
+									<option value={user.id}>
+										{user.username}
+									</option>
+								))}
+							</select>
+						</div>
+					</div>		
+					<div className={styles.call_button}>
+						{callAccepted && !callEnded ? (
+							<Button variant="contained" color="secondary" onClick={leaveCall}>
+								End Call
+							</Button>
+						) : (
+							<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+								<PhoneIcon fontSize="large" />
+							</IconButton>
+						)}
+						{/* {idToCall} */}
+					</div>
+				</div>
+				<div>
+					{receivingCall && !callAccepted ? (
+						<div className={styles.caller}>
+							<h1 style={{fontSize:'22px', width:'350px'}}>{name} is calling...</h1>
+							<Button variant="contained" color="primary" style={{width:'100px', height:'40px', marginTop:'5px'}} onClick={answerCall}>
+								Answer
+							</Button>
+						</div>
+					) : null}
+				</div>
 			</div>
-		</div>
 		</div>
     )
 }
