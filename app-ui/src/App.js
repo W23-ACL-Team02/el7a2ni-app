acc9eb7afeb774581a65b307e14e53d40df6dcf2
ahmedyousry27 <ahmedyousry2002@gmail.com>
2024-01-10 00:09:08 +0100

    some changes

--- app-ui/src/App.js
+++ app-ui/src/App.js
@@ -5,8 +5,10 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import Addprescription from './components-clinic/doctor/addPrescription/Addprescription';
 import ViewPrescriptions from './components-clinic/ViewPrescription';
 import SelectedPrescriptions from './components-clinic/SelectedPrescriptions';
-
-
+import EditMedicine from './components-pharmacy/doctor/EditMedicine';
+import EditSelectedPrescriptions from './components-pharmacy/doctor/EditSelectedPrescriptions';
+import Login from './components-clinic/Login';
+import ViewFamilyMemebrs from './components-clinic/patient/FamilyMembers/ViewFamilyMemebrs'
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
@@ -15,12 +17,24 @@ function App() {
           <BrowserRouter>
           <Link to={'addprescription'}>addPrescription</Link> <br/>
           <Link to={'viewprescription'}>ViewPrescription</Link>
+          <Link to={'editMedicine'}>EditMedicine</Link>
+          <Link to={'login'}>login</Link>
+          <Link to={'viewfamilyMembers'}>familymembers</Link>
+
+
+
 
 
           <Routes>
+          <Route path="/login" element={<Login />} />
           <Route path="/addprescription" element={<Addprescription />} />
           <Route path="/viewprescription" element={<ViewPrescriptions />} />
           <Route path='prescriptions/selectedPrescription' element={<SelectedPrescriptions/>} />
+          <Route path="/editMedicine" element={<EditMedicine />} />
+          <Route path="prescriptions/editSelectedPrescriptions" element={<EditSelectedPrescriptions />} />
+          <Route path="/viewFamilyMembers" element={<ViewFamilyMemebrs />} />
+
+ 
 
 
           
