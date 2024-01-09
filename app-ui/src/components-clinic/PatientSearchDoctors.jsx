bda5d58dab52b0449eeb141d9b58a7494f83abf6
MoKhatib2 <mohamedahmed1902@yahoo.com>
2024-01-10 00:26:26 +0100

    fixed bugs

--- app-ui/src/components-clinic/PatientSearchDoctors.jsx
+++ app-ui/src/components-clinic/PatientSearchDoctors.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import "../css/newTable.css";
 const baseURL = process.env.REACT_APP_SERVER_URL;
 const PatientSearchDoctors = () => {
 
