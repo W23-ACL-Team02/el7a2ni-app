a123414bceafd27a40ea263c03fb2262f8a51e42
Marina-Elwy <114447046+Marina-Elwy@users.noreply.github.com>
2024-01-10 06:20:13 +0100

    front end and backend of new functions , added bookedby in appointment schema front end of old functions , readme file

--- src/models/user.js
+++ src/models/user.js
@@ -261,16 +261,16 @@ const userSchema = new Schema({
       if (this.acceptanceDate==undefined) this.acceptanceDate=Date.now();
       this.acceptanceDate= Date.now()
 
+    },
+    isFamilyMember(familyMemberUsername){
+      if (!this.family || !this.family.linked || this.family.linked.length === 0) {
+        return false; // No family members linked
+      }
+    
+      // Check if the familyMemberUsername exists in the patient's linked family members
+      const foundFamilyMember = this.family.linked.find(member => member.name === familyMemberUsername);
+      return !!foundFamilyMember;
     }
-  },
-  isFamilyMember(familyMemberUsername){
-    if (!this.family || !this.family.linked || this.family.linked.length === 0) {
-      return false; // No family members linked
-    }
-  
-    // Check if the familyMemberUsername exists in the patient's linked family members
-    const foundFamilyMember = this.family.linked.find(member => member.name === familyMemberUsername);
-    return !!foundFamilyMember;
   }
 }
 );
