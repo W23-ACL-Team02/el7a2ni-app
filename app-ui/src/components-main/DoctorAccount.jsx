414e23b1de867a5411ae6db5a4f231dcb00e51dd
MoKhatib2 <mohamedahmed1902@yahoo.com>
2024-01-10 22:31:26 +0100

    added routes

--- app-ui/src/components-main/DoctorAccount.jsx
+++ app-ui/src/components-main/DoctorAccount.jsx
@@ -6,7 +6,7 @@ import Card from './Card'
 import '../css/general.css'
 import '../css/patientHome.css'
 import Logout from './Logout';
-import PatientNavBar from '../components-main/DoctorNavBar'
+import doctorNavBar from '../components-main/DoctorNavBar'
 
 const serverURL = process.env.REACT_APP_SERVER_URL;
 
@@ -28,17 +28,17 @@ export default function DoctorAccount () {
 
     return(
         <div className='main'>
-        <PatientNavBar />
+        <doctorNavBar />
         <div style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', alignContent:'center'}}>
             <div className='top'>
                 <Card height={650} width={350}>
                     <h2 style={{alignSelf: 'center', fontSize: 28, marginTop: 10}}>Account Details</h2>
                     <div className='line' style={{width: 200}}></div>
-                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Username: {patient.username}</p>
-                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Name: {patient.name}</p>
-                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Email: {patient.email}</p>
-                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Mobile: {patient.mobile}</p>
-                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Gender: {patient.gender}</p>
+                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Username: {doctor.username}</p>
+                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Name: {doctor.name}</p>
+                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Email: {doctor.email}</p>
+                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Mobile: {doctor.mobile}</p>
+                    <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Gender: {doctor.gender}</p>
                     <p style={{alignSelf:'center', fontSize: 20, fontWeight: 500, height: 80}}>Date of Birth: {dateOfBirth}</p>
                 </Card>    
                 <div  style={{display:'flex', flexDirection:'column', alignItems:'center', justifyContent:'center', alignContent:'center'}}>
@@ -51,12 +51,14 @@ export default function DoctorAccount () {
                     <Card height={400} width={350} >
                         <h2 style={{alignSelf:'center', fontSize: 28, marginTop: 10}}>Other Actions</h2>
                         <div className='line' style={{width: 200}}></div>
-                        <button style={{width: 200, height: 150, borderRadius: 40, fontSize: 20, marginTop: 10, marginBottom: 20, alignSelf: 'center'}} onClick={() => {navigate('/')}}>Edit Prescriptions</button>
+                        <button style={{width: 200, height: 120, borderRadius: 40, fontSize: 20, marginTop: 10, marginBottom: 20, alignSelf: 'center'}} onClick={() => {navigate('/')}}>Edit Prescriptions</button>
+                        <button style={{width: 200, height: 120, borderRadius: 40, fontSize: 20, marginTop: 10, marginBottom: 20, alignSelf: 'center'}} onClick={() => {navigate('/')}}>Edit Prescriptions</button>
+                        <button style={{width: 200, height: 120, borderRadius: 40, fontSize: 20, marginTop: 10, marginBottom: 20, alignSelf: 'center'}} onClick={() => {navigate('/')}}>Edit Prescriptions</button>
                     </Card> 
                 </div>
             </div>
             <Card height={80} width={750} >
-                <p style={{alignSelf:'center', fontSize: 40, fontWeight: 'bold', height: 80}}>Wallet: {patient.wallet} €</p>
+                <p style={{alignSelf:'center', fontSize: 40, fontWeight: 'bold', height: 80}}>Wallet: {doctor.wallet} €</p>
             </Card> 
         </div>                 
     </div>    
