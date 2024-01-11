180337e875a905e06f4336e31863b20de05b2b59
ahmedyousry27 <ahmedyousry2002@gmail.com>
2024-01-11 07:49:29 +0100

    fix notification pane

--- app-ui/src/components-main/notifications/NotificationPane.jsx
+++ app-ui/src/components-main/notifications/NotificationPane.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import ErrorBox from '../components-pharmacy/ErrorsBox'
+import ErrorBox from '../../components-pharmacy/ErrorsBox'
 import {useNavigate} from 'react-router-dom'
-import Card from './Card'
-import '../css/general.css'
-import '../css/patientHome.css'
+import '../../css/general.css'
+import '../../css/patientHome.css'
+import Card from '../Card'
 
 const baseURL = process.env.REACT_APP_SERVER_URL
 
@@ -12,9 +12,10 @@ export default function NotificationPane() {
     const [notifications, setNotifications] = useState([]);
 
     useEffect(() => {
-        axios.get(`${baseURL}/main/user/notifications`, {withCredentials:true})
+        axios.get(`${baseURL}/main/private/user/notifications`, {withCredentials:true})
             .then((response) => {
                 setNotifications(response.data.notifs)
+                console.log(response.data)
             })
             .catch((error) => {
 
@@ -27,17 +28,16 @@ export default function NotificationPane() {
                 notifications.length < 1 ?
                 <p>No notifications.</p> 
                 :
-                <table>
-                    <tr> {
-                        notifications.map((notification, index) => {
-                            <p key={index}>
-                                <strong>{notification.title}</strong>
-                                <br/>
+                <div>
+                    {
+                        notifications.map((notification, index) => (
+                            <Card>
+                                <strong>{notification.title}</strong> <br />
                                 {notification.message}
-                            </p>
-                        })
-                    } </tr>
-                </table>
+                            </Card>
+                        ))
+                    }
+                </div>
             }  
         </div>
     )
